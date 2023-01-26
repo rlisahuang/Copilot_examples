39f30c549181efeea6f9c6ce105e61c6251243b5
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2023-01-26 12:56:19 -0800

    first bench mark - API requests and plotting

---
+++ README_NEW.md
@@ -0,0 +1,7 @@
+### Data Science
+1. Send a query to some API to obtain some data
+2. Grab the first ten data points from the response, both for the x and the y axes
+-- limitations: bad support for large quantities of data
+3. Plot the data
+4. Notice that the labels on the x axis are too long. 
+5. Go back to step 2 to shorten the data neede for the x axis labels.
---
+++ data_sci.py
@@ -0,0 +1,54 @@
+'''
+Obtain some data from some API. 
+Put the data into a dataframe.
+Then, filter out unwanted data.
+Finally, create a chart with the data.
+
+Problem with the API example: there could be a limit on requests
+'''
+
+import requests 
+import matplotlib.pyplot as plt
+
+
+API_KEY = 'FCR4SVK07P3TOC7D'
+DEMO_API_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+
+def fetch_data(url):
+    # replace the string 'demo' in url with API_KEY
+    url = url.replace('demo', API_KEY)
+    # send a request to the url 
+    '''
+    this prompt failed because none of the snippets worked, so I had to look online how to send a request to the target url
+    Oh! The issue was a missing `requests` module import
+    '''
+
+    # fetch the data from the API
+    response = requests.get(url)
+    # convert the response to json
+    # data = response.json()['Time Series (5min)']
+    # a = response.json()
+    data = list(response.json()['Time Series (5min)'].items())[:10]
+    ''' 
+    modified the line above 
+    '''
+    # the 'high' values of the data points
+    highs = [float(point[1]['2. high']) for point in data] 
+    # get the time portion of the timestamps from data
+    times = [point[0].split(' ')[1] for point in data]
+
+    return times, highs
+
+
+def plot(ts, highs):
+    # plot ts (x) and highs (y)
+    plt.plot(ts, highs)
+    plt.show()
+
+
+def main():
+    ts, highs = fetch_data(DEMO_API_URL)
+    plot(ts, highs)
+    pass
+
+main() 
